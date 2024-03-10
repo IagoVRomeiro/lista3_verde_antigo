a53219dd062f1bdeb8b77b444c3bf23835a71612
IagoVRomeiro <150415909+IagoVRomeiro@users.noreply.github.com>
2024-03-10 18:56:46 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
 # lista3---Marta
+PONTIFÍCIA UNIVERSIDADE CATÓLICA DE MINAS GERAIS
+ Lista 03
+ Professora: Marta Noronha
+ Disciplina: Algoritmos e Estrutura de Dados II
+ Data de entrega: 10/03/2024
+ Requisitos
+ 1. Todos os programas deverão ser desenvolvidos na linguagem de programação Java.
+ 2. Essas práticas poderão ser desenvolvidas em grupos de, no máximo, dois integrantes.
+ 3. Cópias, se existirem, serão encaminhadas ao colegiado de coordenação didática do curso.
+ 4. Fique atento ao charset dos arquivos de entrada e saída. Recomenda-se a utilização dos métodos
+ daclasse MyIO.javaparaleitura de dadosdoteclado. Énecessáriodefinirocharsetaserutilizado
+ antes de começar a leitura de dados do teclado, da seguinte forma:
+ MyIO.setCharset("UTF-8").
+ 5. Assaídas esperadas, cadastradas no VERDE pelo professor, foram geradas empregando-se:
+ System.out.println().
+ 6. Emcada submissão, enviar apenas um arquivo (.java). A regra será necessária para a submissão
+ de exercícios no VERDE e no identificador de plágios utilizado na disciplina.
+ 7. Aresolução (código) de cada exercício deverá ser submetida ao VERDE.
+ 8. AexecuçãodocódigosubmetidoserárealizadaautomaticamentepeloVERDE,masocódigoserá
+ analisado e validado pelo professor.
+ 9. Se for necessário ler o arquivo "pub.in", o mesmo será disponibilizado para download no VERDE
+ juntamente com o arquivo "pub.out".
+ Herança em Java
+ Definição das classes
+ Classe Abstrata Pessoa
+ • DeclararumaclasseabstrataPessoa. AclassePessoaéumaSuperclasseedevepossuirosseguintes
+ atributos:– nome: umacadeia de caracteres (string) com modificador de acesso protegido.– CPF:umatributo do tipo long com modificador de acesso protegido.
+ 1
+Algoritmos e Estrutura de Dados II– Lista 03
+ 2– idade: umatributo do tipo inteiro com modificador de acesso privado.
+ • Implemente o construtor da classe, onde este deve receber o nome, o CPF e a idade por parâmet
+ros. Inicialize as variáveis no construtor por meio dos dados recebidos via parâmetro.
+ • Para os atributos nome e idade, declare e implemente os métodos acessores (getters) e modifi
+cadores (setters).
+ • ParaoatributoCPF,declareummétodoabstratoacessoreoutromodificador. Aodeclará-locomo
+ abstrato, o programador é obrigado a implementar as funções em uma das subclasses.
+ Classe Abstrata Funcionário
+ • Declarar uma classe abstrata Funcionario que deve herdar os métodos e atributos da classe Pes
+soa. Esta classe deve possuir os seguintes atributos:– vinculo: uma cadeia de caracteres (string) com modificador de acesso protegido. Este atrib
+uto serve para separar em subclasses professores e outros funcionários.– horas semanais: um atributo do tipo int com modificador de acesso protegido.
+ • Implemente o construtor da classe, onde este deve receber o vínculo, o nome, o CPF, a idade e as
+ horas semanais que o funcionário trabalha na instituição. Inicialize as variáveis no construtor por
+ meio dos dados recebidos via parâmetro.
+ • Paraoatributo vínculo, declare e implemente o método acessor e modificador de acesso.
+ • Para o atributo horas semanais, declare um método abstrato acessor e outro modificador de
+ acesso. Ao declará-lo como abstrato, o programador é obrigado a implementar as funções em
+ umadassubclasses.
+ Classe Estudante
+ • Declarar uma classe Estudante que deve herdar os métodos e atributos da classe Pessoa. Esta
+ classe deve possuir os seguintes atributos:– vinculo: uma cadeia de caracteres (string).– status: uma cadeia de caracteres (string). Esse atributo armazena se o aluno é veterano ou
+ calouro.– matricula: um atributo do tipo int.– disciplinas: um vetor onde cada posição armazena uma cadeia de caracteres, portanto, um
+ vetor de strings. O vetor armazena as disciplinas que o aluno está matriculado.– total de disciplinas: um atributo do tipo int. Contabiliza o total de disciplinas que o aluno
+ está matriculado.
+ • Implemente o construtor da classe, o qual recebe um vetor de strings contendo os dados do es
+tudante por parâmetro. Inicialize as variáveis no construtor por meio dos dados recebidos via
+ parâmetro.
+ • Para os atributos vínculo, status, matricula e disciplinas, declare e implemente os métodos aces
+sores e modificadores de acesso para cada atributo.
+ • ImplementeométodoacessoremodificadordoCPFnestaclasse. (OBS:Nãoénecessáriodeclarar
+ o atributo CPF nesta classe porque esta classe herda o atributo por herança.
+ • Implemente uma função para imprimir algumas informações solicitadas quando a função for
+ chamada. O formato de impressão é nome, cpf, idade, matrícula, vetor das disciplinas que o aluno
+ está matriculado. Observe o espaçamento entre cada informação.
+ Exemplo: Luis Diogo Ávila de Perez, 79099723492, 38, 795222, Química Orgânica, Algoritmos e Estru
+turas de Dados I, Física I, Ética e Sociedade, Banco de Dados
+ Marta Noronha
+Algoritmos e Estrutura de Dados II– Lista 03
+ 3
+ Classe OutrosFuncionarios
+ • Declarar uma classe OutrosFuncionarios que deve herdar os métodos e atributos da classe Fun
+cionario. Esta classe deve possuir o seguinte atributo:– função: uma cadeia de caracteres (string) com modificador de acesso privado.
+ • Implemente o construtor da classe, o qual recebe um vetor de strings contendo os dados do fun
+cionário por parâmetro. Inicialize as variáveis no construtor por meio dos dados recebidos via
+ parâmetro.
+ • Implemente os métodos acessores e modificadores do CPF e das horas semanais nesta classe.
+ (OBS: Não é necessário declarar os atributos nesta classe porque esta classe herda o atributo por
+ herança.
+ • Paraoatributo função, declare e implemente o método acessor e modificador de acesso.
+ • Implemente uma função para imprimir algumas informações solicitadas quando a função for
+ chamada. Oformato deimpressão é nome, cpf, idade, horas semanais. Observe o espaçamento entre
+ cada informação.
+ Exemplo: Lena Rosângela Carmona de Britto, 79606551557, 50, 40
+ Classe Professor
+ • DeclararumaclasseProfessor quedeveherdarosmétodoseatributosdaclasseFuncionario. Esta
+ classe deve possuir os seguintes atributos:– disciplinas: um vetor onde cada posição armazena uma cadeia de caracteres, portanto, um
+ vetor de strings. O vetor armazena as disciplinas que o professor leciona na instituição.– total de disciplinas: um atributo do tipo int.
+ • Implemente o construtor da classe, o qual recebe um vetor de strings contendo os dados do pro
+fessor por parâmetro. Inicialize as variáveis no construtor por meio dos dados recebidos via
+ parâmetro.
+ • Implemente os métodos acessores e modificadores do CPF e das horas semanais nesta classe.
+ (OBS: Não é necessário declarar os atributos nesta classe porque esta classe herda o atributo por
+ herança.
+ • Implemente o método acessor e modificador das disciplinas.
+ • Implemente uma função para imprimir algumas informações solicitadas quando a função for
+ chamada. O formato de impressão é nome, cpf, idade, horas semanais, disciplinas lecionadas. Observe
+ o espaçamento entre cada informação.
+ Exemplo: José Assunção de Delgado, 88443157238, 56, 12, Química, Química Orgânica, Química In
+orgânica
+ Classe Main
+ • Inicialize um vetor de objetos da classe para cada uma das classes:– classe Estudante: O vetor deve conter 70 posições;– classe Outros Funcionarios: O vetor deve conter 60 posições;– classe Professor: o vetor deve conter 60 posições.
+ • Leitura do arquivo pub.in: Leia o arquivo até encontrar a palavra "FIM". Neste arquivo, os dados
+ de cada pessoa estão separados por uma vírgula. Para cada pessoa, verifique:– Seapessoa for um estudante, armazene os dados na classe Estudante.
+ Marta Noronha
+Algoritmos e Estrutura de Dados II– Lista 03
+ 4– Seapessoa for um professor, armazene os dados na classe Professor.– Seapessoa for outro funcionário, armazene os dados na classe OutrosFuncionarios.
+ • Apósfinalizar a leitura do arquivo pub.in, utilize a função imprimir declarada em cada uma das
+ subclasses (Estudante, Professor, OutrosFuncionarios) para imprimir o conteúdo de cada vetor
+ de objetos destas classes. Imprima primeiramente o vetor de objetos da classe Estudante, após
+ imprima o vetor da classe OutrosFuncionarios e, por fim, o vetor da classe Professores. (OBS:
+ Fique atento para não acessar posições inválidas ou não inicializadas ao realizar a leitura.)
+ DICAS:- Para repassar os atributos recebidos em cada subclasse para a superclasse, utilize a função super(var1,
+ var2,...,varn).- Utilize extends para conectar as classes por herança. Ex: class x extends z..., onde x é a subclasse e z é
+ umasuperclasse.
+ Marta Noronha
--- lista3.java
+++ lista3.java
@@ -1,34 +1,36 @@
 import java.util.Scanner;
 
-
 abstract class Pessoa {
     protected String nome;
     protected long CPF;
     private int idade;
 
-    //construtor
     public Pessoa(String nome, long CPF, int idade) {
         this.nome = nome;
         this.CPF = CPF;
         this.idade = idade;
     }
-   
-    //idade
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    
-    //cpf
-    public abstract long getCPF();
-    public abstract void setCPF(long CPF);
-    
-    //nome
+
+    public long getCPF(){
+        return CPF;
+    }
+
+    public void setCPF(long CPF){
+        this.CPF = CPF;
+    }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -38,24 +40,27 @@ abstract class Funcionario extends Pessoa {
     protected String vinculo;
     protected int horasSemanais;
 
-    //construtor
     public Funcionario(String vinculo, String nome, long CPF, int idade, int horasSemanais) {
         super(nome, CPF, idade);
         this.vinculo = vinculo;
         this.horasSemanais = horasSemanais;
     }
 
-    //vinculo
     public String getVinculo() {
         return vinculo;
     }
+
     public void setVinculo(String vinculo) {
         this.vinculo = vinculo;
     }
 
-    //horasSemanais
-    public abstract int getHorasSemanais();
-    public abstract void setHorasSemanais(int horasSemanais);
+    public int getHorasSemanais(){
+        return horasSemanais;
+    }
+
+    public void setHorasSemanais(int horasSemanais){
+        this.horasSemanais = horasSemanais;
+    }
 }
 
 class Estudante extends Pessoa {
@@ -63,111 +68,76 @@ class Estudante extends Pessoa {
     private String status;
     private int matricula;
     private String[] disciplinas;
-    private int totalDisciplinas;
 
-    //construtor
-    public Estudante(String nome, long CPF, int idade, String vinculo, String status, int matricula, String[] disciplinas, int totalDisciplinas) {
+    public Estudante(String nome, long CPF, int idade, String vinculo, String status, int matricula, String[] disciplinas) {
         super(nome, CPF, idade);
         this.vinculo = vinculo;
         this.status = status;
         this.matricula = matricula;
         this.disciplinas = disciplinas;
-        this.totalDisciplinas = totalDisciplinas;
     }
 
-    //vinculo
     public String getVinculo() {
         return vinculo;
     }
+
     public void setVinculo(String vinculo) {
         this.vinculo = vinculo;
     }
 
-    //status
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
 
-    //matricula
     public int getMatricula() {
         return matricula;
     }
+
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
 
-    //disciplinas
     public String[] getDisciplinas() {
         return disciplinas;
     }
+
     public void setDisciplinas(String[] disciplinas) {
         this.disciplinas = disciplinas;
     }
 
-    //totalDisciplinas
-    public int getTotalDisciplinas() {
-        return totalDisciplinas;
-    }
-    public void setTotalDisciplinas(int totalDisciplinas) {
-        this.totalDisciplinas = totalDisciplinas;
-    }
 
-    //CPF
-    public long getCPF() {
-        return this.CPF;
-    }
-    public void setCPF(long CPF) {
-        this.CPF = CPF;
-    }
-
-    //imprimir Estudante
     public void imprimir() {
-        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + vinculo + ", " + status + ", " + matricula + ", " + VetorParaString(disciplinas));
+        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + matricula);
+        for(int i = 0; i < disciplinas.length; i++){
+            System.out.printf(", " + disciplinas[i]);
+        }
         System.out.println();
     }
-
-
 }
 
 class OutrosFuncionarios extends Funcionario {
     private String funcao;
 
-    //construtor
     public OutrosFuncionarios(String nome, long CPF, int idade, String vinculo, int horasSemanais, String funcao) {
         super(vinculo, nome, CPF, idade, horasSemanais);
         this.funcao = funcao;
     }
 
-    //função
     public String getFuncao() {
         return funcao;
     }
+
     public void setFuncao(String funcao) {
         this.funcao = funcao;
     }
 
-    //HorasSemanais
-    public int getHorasSemanais() {
-        return this.horasSemanais;
-    }
-    public void setHorasSemanais(int horasSemanais) {
-        this.horasSemanais = horasSemanais;
-    }
 
-    //CPF
-    public long getCPF() {
-        return this.CPF;
-    }
-    public void setCPF(long CPF) {
-        this.CPF = CPF;
-    }
-
-    //imprimir outrosfunc
     public void imprimir() {
-        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + horasSemanais + ", " + vinculo);
+        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + horasSemanais);
         System.out.println();
     }
 }
@@ -175,39 +145,26 @@ class OutrosFuncionarios extends Funcionario {
 class Professor extends Funcionario {
     private String[] disciplinas;
 
-    //construtor
     public Professor(String nome, long CPF, int idade, String vinculo, String[] disciplinas, int horasSemanais) {
         super(vinculo, nome, CPF, idade, horasSemanais);
         this.disciplinas = disciplinas;
     }
 
-    //disciplinas
     public String[] getDisciplinas() {
         return disciplinas;
     }
+
     public void setDisciplinas(String[] disciplinas) {
         this.disciplinas = disciplinas;
     }
 
-    //horasSemanais
-    public int getHorasSemanais() {
-        return this.horasSemanais;
-    }
-    public void setHorasSemanais(int horasSemanais) {
-        this.horasSemanais = horasSemanais;
-    }
 
-    //CPF
-    public long getCPF() {
-        return this.CPF;
-    }
-    public void setCPF(long CPF) {
-        this.CPF = CPF;
-    }
-
-    //imprimir Professor
     public void imprimir() {
-        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + horasSemanais + ", " + VetorParaString(disciplinas));
+        System.out.printf(nome + ", " + CPF + ", " + getIdade() + ", " + horasSemanais);
+        for(int i = 0; i < disciplinas.length; i++){
+
+            System.out.printf(", " + disciplinas[i]);
+        }
         System.out.println();
     }
 }
@@ -215,13 +172,12 @@ class Professor extends Funcionario {
 public class lista3 {
 
     public static void main(String[] args) {
-        // Criação de vetores de objetos
         Estudante[] estudantes = new Estudante[70];
         OutrosFuncionarios[] outrosFuncionarios = new OutrosFuncionarios[60];
         Professor[] professores = new Professor[60];
 
-        // Leitura do arquivo pub.in
         Scanner ler = new Scanner(System.in);
+
         while (ler.hasNextLine()) {
             String linha = ler.nextLine();
             if (linha.equals("FIM")) {
@@ -238,82 +194,74 @@ public class lista3 {
                 int idade = Integer.parseInt(info[3].trim());
                 String status = info[4].trim();
                 int matricula = Integer.parseInt(info[5].trim());
-                String[] disciplinas = new String[info.length - 5];
+                String[] disciplinas = new String[info.length - 6];
                 for (int i = 6; i < info.length; i++) {
                     disciplinas[i - 6] = info[i].trim();
                 }
-                int disciplinasTotal = info.length - 5;
-                
-                Estudante estudante = new Estudante(nome, CPF, idade, vinculo, status, matricula, disciplinas, disciplinasTotal);
 
-                //add o estudante ao vetor
+                Estudante estudante = new Estudante(nome, CPF, idade, vinculo, status, matricula, disciplinas);
+
                 for (int i = 0; i < estudantes.length; i++) {
                     if (estudantes[i] == null) {
                         estudantes[i] = estudante;
                         break;
                     }
                 }
-            } 
-           
-            if(info[0].trim().equals("Funcionario")){
-             if (info[1].trim().equals("Outros Funcionarios")){
-                String vinculo = info[1].trim();
-                String nome = info[2].trim();
-                long CPF = Long.parseLong(info[3].trim());
-                int idade = Integer.parseInt(info[4].trim());
-                String funcao = info[5].trim();
-                int horasSemanais = Integer.parseInt(info[6].trim());
-
-                OutrosFuncionarios outroFuncionario = new OutrosFuncionarios(nome, CPF, idade, vinculo, horasSemanais, funcao);
-                //add funcionario ao vetor
-                for (int i = 0; i < outrosFuncionarios.length; i++) {
-                    if (outrosFuncionarios[i] == null) {
-                        outrosFuncionarios[i] = outroFuncionario;
-                        break;
-                    }
-                }
-            } 
-             else if (info[1].trim().equals("Professor")){
-                String vinculo = info[1].trim();
-                String nome = info[2].trim();
-                long CPF = Long.parseLong(info[3].trim());
-                int idade = Integer.parseInt(info[4].trim());
-                String[] disciplinas = new String[info.length - 6];
-                int horasSemanais = Integer.parseInt(info[info.length].trim());
-
-                for (int i = 5; i < info.length - 1; i++) {
-                    disciplinas[i - 5] = info[i].trim();                   
-                }
-
-                Professor professor = new Professor(nome, CPF, idade, vinculo, disciplinas, horasSemanais);
-                // add o professor ao vetor
-                for (int i = 0; i < professores.length; i++) {
-                    if (professores[i] == null) {
-                        professores[i] = professor;
-                        break;
-                    }
-                }
             }
+
+            if(info[0].trim().equals("Funcionario")){
+                if (info[1].trim().equals("OutrosFuncionarios")){
+                    String vinculo = info[1].trim();
+                    String nome = info[2].trim();
+                    long CPF = Long.parseLong(info[3].trim());
+                    int idade = Integer.parseInt(info[4].trim());
+                    String funcao = info[5].trim();
+                    int horasSemanais = Integer.parseInt(info[6].trim());
+
+                    OutrosFuncionarios outroFuncionario = new OutrosFuncionarios(nome, CPF, idade, vinculo, horasSemanais, funcao);
+                    for (int i = 0; i < outrosFuncionarios.length - 1; i++) {
+                        if (outrosFuncionarios[i] == null) {
+                            outrosFuncionarios[i] = outroFuncionario;
+                            break;
+                        }
+                    }
+                }
+                else if (info[1].trim().equals("Professor")){
+                    String vinculo = info[1].trim();
+                    String nome = info[2].trim();
+                    long CPF = Long.parseLong(info[3].trim());
+                    int idade = Integer.parseInt(info[4].trim());
+                    String[] disciplinas = new String[info.length - 6];
+                    int horasSemanais = Integer.parseInt(info[info.length - 1].trim());
+
+                    for (int i = 5; i < info.length - 1; i++) {
+                        disciplinas[i - 5] = info[i].trim();
+                    }
+
+                    Professor professor = new Professor(nome, CPF, idade, vinculo, disciplinas, horasSemanais);
+                    for (int i = 0; i < professores.length; i++) {
+                        if (professores[i] == null) {
+                            professores[i] = professor;
+                            break;
+                        }
+                    }
+                }
             }
         }
-        
-    
-        //imprimir dados
-        System.out.println("Estudantes:");
+
+
         for (Estudante estudante : estudantes) {
             if (estudante != null) {
                 estudante.imprimir();
             }
         }
 
-        System.out.println("\nOutros Funcionários:");
         for (OutrosFuncionarios outrosFuncionario : outrosFuncionarios) {
             if (outrosFuncionario != null) {
                 outrosFuncionario.imprimir();
             }
         }
 
-        System.out.println("\nProfessores:");
         for (Professor professor : professores) {
             if (professor != null) {
                 professor.imprimir();
@@ -323,14 +271,3 @@ public class lista3 {
     }
 
 }
-    //converter array de strings em uma unica string
-    private String VetorParaString(String[] array) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            sb.append(array[i]);
-            if (i != array.length - 1) {
-                sb.append(", ");
-            }
-        }
-        return sb.toString();
-    }
